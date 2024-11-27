2eaece0e727d2158ddd12e003ac85bfbca8b201d
5mitty <43888178+5mitty@users.noreply.github.com>
2024-11-27 09:36:26 -0800

    rewrote main to implement about page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,16 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-import App from './App'
-import HomePage from './pages/Home'
-import ErrorPage from './pages/Error'
+import App from './App';
+import HomePage from './pages/Home';
+import ErrorPage from './pages/Error';
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'YOUR_GRAPHQL_ENDPOINT', // replace with your GraphQL endpoint
+  cache: new InMemoryCache(),
+});
 
 const router = createBrowserRouter([
   {
@@ -14,12 +21,16 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <HomePage />,
-      }
+      },
     ],
   },
 ]);
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+  ReactDOM.createRoot(rootElement).render(
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
+  );
 }
