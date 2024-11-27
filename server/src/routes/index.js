724b73a3e86bcd3b0314720a310c3ff491cfc161
5mitty <43888178+5mitty@users.noreply.github.com>
2024-11-27 12:23:49 -0800

    import spotifyAuth and created route for spotifyAuth

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -6,10 +6,15 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import apiRoutes from './api/index.js';
+import spotifyAuth from './spotify/spotifyAuth.js'; // Import the spotifyAuth route
 
+// Use the spotifyAuth route under /api/spotify
+router.use('/api/spotify', spotifyAuth);
+
+// Use the other API routes
 router.use('/api', apiRoutes);
 
-// serve up react front-end in production
+// Serve up react front-end in production
 router.use((_req, res) => {
   res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
 });
