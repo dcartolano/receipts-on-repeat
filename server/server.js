63d80215e53bcd1e4062c3628900a85adaaa4104
5mitty <43888178+5mitty@users.noreply.github.com>
2024-12-01 22:47:43 -0600

    updated /routes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import spotifyAuth from './routes/spotifyAuth.js'; // Adjusted import path
+import spotifyAuth from './routes/spotify/spotifyAuth.js';
 import dotenv from 'dotenv';
 import path from 'path';
+import routes from './routes/index.js'
 
 dotenv.config();
 
@@ -13,15 +14,16 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files from the React app
-app.use(express.static('client/build')); // Adjust the path to your build folder
-
 // Use the Spotify authentication routes
-app.use('/spotify', spotifyAuth);
+//app.use('/spotify', spotifyAuth);
 
+// Serve static files from the React app
+app.use(express.static(path.join(process.cwd(), 'client/build'))); // Use process.cwd() for better compatibility
+
+app.use(routes);
 // Catch-all route to serve the React app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html')); // Adjust the path as necessary
+    res.sendFile(path.join(process.cwd(), 'client/build', 'index.html')); // Use process.cwd() for better compatibility
 });
 
 // Start the server
