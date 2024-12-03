b37f55cb466aea1ae7715775b9bdc9ef60aa4b00
5mitty <43888178+5mitty@users.noreply.github.com>
2024-12-03 16:54:03 -0600

    added selectedPlaylist from localStorage and added PlaylistReceipt handling

--- client/src/pages/PlaylistReceipt.jsx
+++ client/src/pages/PlaylistReceipt.jsx
@@ -1,12 +1,18 @@
+import React from 'react';
 import Receipt from "../components/Receipt/index.jsx";
 
 const PlaylistReceipt = () => {
+    // Fetch user data from local storage
     const userData = JSON.parse(localStorage.getItem('userData'));
+    
+    // Retrieve the selected playlist from local storage
+    const selectedPlaylist = JSON.parse(localStorage.getItem('selectedPlaylist')); 
+
     return (
         <div>
-            <Receipt />
+            <Receipt playlist={selectedPlaylist} userData={userData} /> {/* Pass the selected playlist and user data to Receipt */}
         </div>
-    )
+    );
 }
 
 export default PlaylistReceipt;
