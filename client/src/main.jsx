bb541e9192582d0ce0a7979b3acc89f5e3bcf05a
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-12-03 11:05:52 -0600

    updated playlistreceipt and userprofile imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import HomePage from './pages/HomePage'; // Ensure the path is correct
 import ErrorPage from './pages/Error';
-import PlaylistReceipt from './pages/playlistReceipt.jsx';
-import UserProfile from './pages/userProfile.jsx';
+import PlaylistReceipt from './pages/PlaylistReceipt';
+import UserProfile from './pages/UserProfile';
 
 const router = createBrowserRouter([
     {
