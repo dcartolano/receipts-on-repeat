ba537f018ea557da7a1da4e54d9ac17f0f9ceee4
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-12-03 10:57:19 -0600

    added in comments with original app.use statements

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import spotifyAuth from './routes/spotify/spotifyAuth.js';
 import dotenv from 'dotenv';
 import path from 'path';
 import routes from './routes/index.js'
+// import path from 'node:path'
+// const __dirname = import.meta.dirname;
 
 dotenv.config();
 
@@ -20,11 +22,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files from the React app
 app.use(express.static(path.join(process.cwd(), 'client/build'))); // Use process.cwd() for better compatibility
 
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/dist')));
+// }
+
 app.use(routes);
 // Catch-all route to serve the React app
 app.get('*', (req, res) => {
     res.sendFile(path.join(process.cwd(), 'client/build', 'index.html')); // Use process.cwd() for better compatibility
 });
+
+// app.get('*', (_req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+//   });
+
 
 // Start the server
 app.listen(PORT, () => {
