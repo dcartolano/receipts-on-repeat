fee47d6e6135a4411d2d711684f52a724fc57e5c
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-12-03 10:56:04 -0600

    included new user profile and playlist receipt pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import HomePage from './pages/HomePage'; // Ensure the path is correct
 import ErrorPage from './pages/Error';
+import PlaylistReceipt from './pages/playlistReceipt.jsx';
+import UserProfile from './pages/userProfile.jsx';
 
 const router = createBrowserRouter([
     {
@@ -14,6 +16,14 @@ const router = createBrowserRouter([
             {
                 index: true,
                 element: <HomePage />,
+            },
+            {
+                index: '/playlistReceipt',
+                element: <PlaylistReceipt />,
+            },
+            {
+                index: '/userProfile',
+                element: <UserProfile />,
             },
         ],
     },
