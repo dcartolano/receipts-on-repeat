a5bf876acfb5d7425c5dbb5933683f53929d6fc9
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-12-04 15:16:41 -0600

    edited footer content

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,15 +1,15 @@
-import { Link } from 'react-router-dom';
-import logo from '../../assets/receipt-printer-for-logo.jpeg';
+// import { Link } from 'react-router-dom';
+// import logo from '../../assets/receipt-printer-for-logo.jpeg';
 import spotifyLogo from '../../assets/Spotify_Full_Logo_RGB_Green.png';
+import lyricsOvhLogo from '../../assets/lyrics-ovh-logo.png';
 
 const Footer = () => {
     return (
         <footer>
-            <div className="header-box">
-                <div className="header-box-inside">
-                    <img src={logo} alt="Application Logo" className="application-image"/>
-                    <p className="application-title">Receipts on Repeat</p>
-                    <p className="external-api-logo">Music content provided by <a href="https://open.spotify.com" target="_blank" ><img src={spotifyLogo} alt="" className="external-api-logo"/></a></p>
+            <div className="footer-box">
+                <div className="footer-box-inside">
+                    <p className="text-left">Powered by <a href="https://open.spotify.com" target="_blank" ><img src={spotifyLogo} alt="" className="external-api-logo"/></a></p>
+                    <p className="text-right">Lyrics data courtesy of <a href="https://lyrics.ovh" target="_blank" ><img src={lyricsOvhLogo} alt="" className="external-api-logo"/></a></p>
                 </div>
             </div>
         </footer>
