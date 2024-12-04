b984c0c78aa6dcc6835ef44727c342101db082a7
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-12-03 23:16:19 -0600

    added api route for get request that isn't currently returning the correct information

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -1,9 +1,34 @@
 export const createReceipt = (receiptData) => {
-    return fetch('/spotify/receipts', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(receiptData),
-    });
-  };
+  return fetch('/spotify/receipts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(receiptData),
+  });
+};
+
+export const getAllReceipts =  () => {
+  // try {
+  //   const response =  fetch('/spotify/receipts');
+  //   if (!response.ok) {
+  //     throw new Error('Network response was not ok');
+  //   }
+  //   // const data = await response.json();
+  //   // console.log(data);
+  //   // console.log(data.receipts);
+  //   // return data;
+  //   return response;
+  // } catch (error) {
+  //   console.error('Failed to fetch questions:', error);
+  //   throw error;
+  // }
+  return fetch('/spotify/receipts'
+    , {
+      method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+)
+};
