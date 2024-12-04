1b11f39392b33fd6481570941ecc8d4fcce23e88
5mitty <43888178+5mitty@users.noreply.github.com>
2024-12-04 17:32:10 -0600

    added div around logo link and a div around github text links

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -8,8 +8,18 @@ const Footer = () => {
         <footer>
             <div className="footer-box">
                 <div className="footer-box-inside">
-                    <p className="text-left">Powered by <a href="https://open.spotify.com" target="_blank" ><img src={spotifyLogo} alt="" className="external-api-logo"/></a></p>
-                    <p className="text-right">Lyrics data courtesy of <a href="https://lyrics.ovh" target="_blank" ><img src={lyricsOvhLogo} alt="" className="external-api-logo"/></a></p>
+                    <div className='footer-logo'>
+                        <p className="text-left spotify-box">Powered by <a href="https://open.spotify.com" target="_blank" ><img src={spotifyLogo} alt="" className="external-api-logo lyrics-logo"/></a></p>
+                        <p className="text-right lyrics-box">Lyrics data courtesy of <a href="https://lyrics.ovh" target="_blank" ><img src={lyricsOvhLogo} alt="" className="external-api-logo spotify-logo"/></a></p>
+                    </div>
+                    <div className='footer-creators-text'>
+                        <p className='creators-text'>
+                            Created by <a href="https://github.com/dcartolano" target="_blank" className='creator-text-link'>David Cartolano</a>&nbsp;
+                        </p>
+                        <p className='creators-text'>
+                            and <a href="https://github.com/5mitty" target="_blank" className='creator-text-link'>Jacob Smith</a>
+                        </p>
+                    </div>
                 </div>
             </div>
         </footer>
