84cf5405aceaad0efaf98715fcaf58ad00b6ca65
5mitty <43888178+5mitty@users.noreply.github.com>
2024-12-04 13:43:34 -0600

    added entryPage with welcome-box and entry-button

---
+++ client/src/pages/EntryPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const EntryPage = () => {
+   const navigate = useNavigate();
+
+   const handleNavigate = () => {
+       navigate('/homePage'); // Navigate to HomePage
+   };
+
+   return (
+       <div className='entry-page-welcome-box-outer'>
+           <div className='entry-page-welcome-box'>
+               <h1 className='entry-page-text'>Welcome to Reciepts on Repeat, we love music and hope you do to! Our purpose was to create a way of viewing music playlists that is enjoyable through and through</h1>
+               <button onClick={handleNavigate} className='entry-button'>Start My Journey</button>
+           </div>
+       </div>
+   );
+};
+
+export default EntryPage;
