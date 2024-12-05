83d76fcd56d491b84bf31f712ac7761d35bb3230
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-12-04 21:11:57 -0600

    added login to nav bar, made page title a link, and made all the paths camel case

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -7,11 +7,13 @@ const Header = () => {
             <div className="header-box">
                 <div className="header-box-inside">
                     <img src={logo} alt="Application Logo" className="application-image"/>
-                    <p className="application-title">Receipts on Repeat</p>
+                    {/* <p className="application-title">Receipts on Repeat</p> */}
+                    <Link to="/" className="application-title">Receipts on Repeat</Link>
                     <nav className="nav-bar">
-                        <Link to="/UserProfile" className="nav-bar-link">User Profile</Link>
-                        <Link to="/PlaylistReceipt" className="nav-bar-link">Current Playlist</Link>
-                        <Link to="/SavedPlaylists" className="nav-bar-link">Saved Playlists</Link>
+                        <Link to="/login" className="nav-bar-link">Login</Link>
+                        <Link to="/userProfile" className="nav-bar-link">User Profile</Link>
+                        <Link to="/playlistReceipt" className="nav-bar-link">Current Playlist</Link>
+                        <Link to="/savedPlaylists" className="nav-bar-link">Saved Playlists</Link>
                     </nav>
                 </div>
             </div>
