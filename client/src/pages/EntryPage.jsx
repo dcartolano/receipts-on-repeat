6d2994d45d53d512817e627a004c827ac8287256
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-12-04 21:03:06 -0600

    added additional options for text, commented out unused options

--- client/src/pages/EntryPage.jsx
+++ client/src/pages/EntryPage.jsx
@@ -11,8 +11,13 @@ const EntryPage = () => {
    return (
        <div className='entry-page-welcome-box-outer'>
            <div className='entry-page-welcome-box'>
-               <h1 className='entry-page-text'>Welcome to Reciepts on Repeat. We love music and hope you do to! Our purpose was to create a way of viewing music playlists that is enjoyable through and through</h1>
-               <button onClick={handleNavigate} className='entry-button'>Start My Journey</button>
+               {/* <h1 className='entry-page-text'>Welcome to Reciepts on Repeat. We love music and hope you do to! Our purpose was to create a way of viewing music playlists that is enjoyable through and through</h1> */}
+               <h1 >Welcome to Receipts on Repeat! </h1>
+               <h2 className='entry-page-text'>We're Jake and Dave, a couple of coding students who made this web app to give you a fun new way to visualize your playlists- all you need is a Spotify account to get started.</h2>
+               <p>And if signing into your Spotify through a random web app freaks you out a little bit, no worries, we totally get it- that's why we designed this app so only you will have access to the data Spotify provides, unless you decide to post one of your playlists to the public "Saved Playlists" page.</p>
+               {/* <h2 className='entry-page-text'>Thanks for visiting, and we hope you enjoy this app as much as we have building it!</h2> */}
+               <p>Thanks for visiting, and we hope you'll enjoy this app as much as we do.</p>
+               <button onClick={handleNavigate} className='entry-button'>Click here to begin!</button>
            </div>
        </div>
    );
