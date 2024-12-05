fb1969ccf30ae2131449d6e2ba19f4c556c16f72
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-12-05 13:01:57 -0600

    removed unused imports

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,5 +1,3 @@
-// import { Link } from 'react-router-dom';
-// import logo from '../../assets/receipt-printer-for-logo.jpeg';
 import spotifyLogo from '../../assets/Spotify_Full_Logo_RGB_Green.png';
 import lyricsOvhLogo from '../../assets/lyrics-ovh-logo.png';
 
@@ -9,8 +7,8 @@ const Footer = () => {
             <div className="footer-box">
                 <div className="footer-box-inside">
                     <div className='footer-logo'>
-                        <p className="text-left spotify-box">Powered by <a href="https://open.spotify.com" target="_blank" ><img src={spotifyLogo} alt="" className="external-api-logo lyrics-logo"/></a></p>
-                        <p className="text-right lyrics-box">Lyrics data courtesy of <a href="https://lyrics.ovh" target="_blank" ><img src={lyricsOvhLogo} alt="" className="external-api-logo spotify-logo"/></a></p>
+                        <p className="text-left spotify-box">Powered by <a href="https://open.spotify.com" target="_blank" ><img src={spotifyLogo} alt="" className="external-api-logo lyrics-logo" /></a></p>
+                        <p className="text-right lyrics-box">Lyrics data courtesy of <a href="https://lyrics.ovh" target="_blank" ><img src={lyricsOvhLogo} alt="" className="external-api-logo spotify-logo" /></a></p>
                     </div>
                     <div className='footer-creators-text'>
                         <p className='creators-text'>
