35e257ce6ec59352b7f750d0359871189163f9c0
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-12-04 21:14:29 -0600

    removed unused minutes functioinality from timeSum function, added useState for showing the button, added function to handle button click, changed logic for button and comment

--- client/src/components/Receipt/index.jsx
+++ client/src/components/Receipt/index.jsx
@@ -1,30 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import { createReceipt } from '../../utils/Api';
 
 const Receipt = ({ playlist }) => {
     const [userData, setUserData] = useState(null);
+    const [showButton, setShowButton] = useState(true);
 
     const timeSum = (previousTotal, timeToAdd) => {
         const time1 = previousTotal.split('.');
         const time2 = timeToAdd.split('.');
-        
+
         let secondSum = Number(time1[1]) + Number(time2[1]);
         let minSum = Number(time1[0]) + Number(time2[0]);
-        
-        if(secondSum > 59){
-          secondSum = Math.abs(60 - secondSum);
-          minSum += 1;
+
+        if (secondSum > 59) {
+            secondSum = Math.abs(60 - secondSum);
+            minSum += 1;
         }
-        
-        if(secondSum < 10){
-          secondSum = `0${secondSum}`;
+
+        if (secondSum < 10) {
+            secondSum = `0${secondSum}`;
         }
-        
-        // if(minSum < 10){
-        //   minSum = `0${minSum}`;
-        // }
-        
-        return `${minSum}.${secondSum}`;   
+
+        return `${minSum}.${secondSum}`;
     }
+    // console.log(playlist)
+    const handleSaveReceiptClick = async () => {
+        try {
+            const response = await createReceipt(playlist);
+            // console.log(response);
+            // if (!response.ok) {
+            //     throw new Error('something went wrong!');
+            // }
+        } catch (err) {
+            console.error(err);
+        };
+        setShowButton(false);
+    };
 
     useEffect(() => {
         // Fetch user data from local storage
@@ -38,41 +49,41 @@ const Receipt = ({ playlist }) => {
     }, []);
 
     const renderPlaylist = (playlist) => {
-    let totalPrice = '0.00'; // Initialize total price for the current playlist
+        let totalPrice = '0.00'; // Initialize total price for the current playlist
 
-    return (
-        <div>
-            <div className='playlist-outer'>
-                <div key={playlist.id} className="playlist-container border p-3 mt-3 receipt-font">
-                    <img src={playlist.imageUrl} alt="Playlist image from Spotify" className="playlist-image" />
-                    <h3 className='receipt-playlist-name'>{playlist.name}</h3>
-                    <ul id="songList" className="list-unstyled">
-                        {playlist.tracks.map((track) => {
-                            totalPrice = timeSum(totalPrice, track.duration);
-                            return (
-                                <li key={track.id} className="song-item d-flex justify-content-between align-items-end">
-                                    <div className="text-left wrap-indent">
-                                        <span>{track.name}</span>
-                                        <span className="mx-2 artist-font">by {track.artist || 'Unknown Artist'}</span>
-                                    </div>
-                                    <span className="text-right">${track.duration}</span>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                    <h4>Total: ${totalPrice}</h4>
-                    {playlist.lyrics && (
-                        <p style={{ fontSize: '0.8em' }}>
-                            "{playlist.lyrics.lyrics}" - {playlist.lyrics.artist}
-                        </p>
-                    )}
-                    <div className='codes-section'>
-                        <img src={playlist.qrCode} alt="Scannable QR Code with link to the playlist" className="codes qr-code" />
-                        <img src={playlist.spotifyCode} alt="Scannable Spotify Code with link to the playlist" className="codes spotify-scannable-code" />
+        return (
+            <div>
+                <div className='playlist-outer'>
+                    <div key={playlist.id} className="playlist-container border p-3 mt-3 receipt-font">
+                        <img src={playlist.imageUrl} alt="Playlist image from Spotify" className="playlist-image" />
+                        <h3 className='receipt-playlist-name'>{playlist.name}</h3>
+                        <ul id="songList" className="list-unstyled">
+                            {playlist.tracks.map((track) => {
+                                totalPrice = timeSum(totalPrice, track.duration);
+                                return (
+                                    <li key={track.id} className="song-item d-flex justify-content-between align-items-end">
+                                        <div className="text-left wrap-indent">
+                                            <span>{track.name}</span>
+                                            <span className="mx-2 artist-font">by {track.artist || 'Unknown Artist'}</span>
+                                        </div>
+                                        <span className="text-right">${track.duration}</span>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                        <h4>Total: ${totalPrice}</h4>
+                        {playlist.lyrics && (
+                            <p style={{ fontSize: '0.8em' }}>
+                                "{playlist.lyrics.lyrics}" - {playlist.lyrics.artist}
+                            </p>
+                        )}
+                        <div className='codes-section'>
+                            <img src={playlist.qrCode} alt="Scannable QR Code with link to the playlist" className="codes qr-code" />
+                            <img src={playlist.spotifyCode} alt="Scannable Spotify Code with link to the playlist" className="codes spotify-scannable-code" />
+                        </div>
                     </div>
                 </div>
-            </div>
-            {/* <div className="text-center mt-3">
+                {/* <div className="text-center mt-3">
                 {isEditing ? (
                     <div>
                         <textarea
@@ -90,16 +101,20 @@ const Receipt = ({ playlist }) => {
                     </div>
                 )}
             </div> */}
-            <div>
-                <p className='playlist-comment'>{playlist.comment}</p>
-                {/* <button onClick={handleEditClick} className="btn btn-secondary">Edit Comment</button> */}
+                <div>
+                    {/* <p className='playlist-comment'>{playlist.comment}</p> */}
+                    { showButton ? (
+                    <button onClick={handleSaveReceiptClick} className="btn btn-secondary">Post Receipt to "Saved Playlists" Page</button>
+                ): (
+                    <p>Playlist saved!</p>
+                )}
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    };
 
     return (
-        
+
         <div className="container mt-5">
             <div className='welcomeUser-outer'>
                 <div className='welcomeUser'>
